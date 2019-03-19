40658d3093b8a02e526d1936d972740b0ec1c76b
yrachid <anuar.yasser@gmail.com>
2019-03-19 11:15:20 -0300

    Classes iniciais da mecanica

---
+++ src/main/java/com/thoughtworks/aceleradora/oo/mecanica/Mecanica.java
@@ -0,0 +1,13 @@
+package com.thoughtworks.aceleradora.oo.mecanica;
+
+public class Mecanica {
+
+    public Carro montarCarro() {
+
+        Motor motorV8 = new Motor();
+        Freio freioAbs = new Freio();
+
+        return null;
+    }
+
+}
---
+++ src/main/java/com/thoughtworks/aceleradora/oo/mecanica/Motor.java
@@ -0,0 +1,4 @@
+package com.thoughtworks.aceleradora.oo.mecanica;
+
+public class Motor {
+}
